e908769a724d262b0c92cbc7974d24931671929e
SamzoGuo <53573565+SamzoGuo@users.noreply.github.com>
2019-10-30 10:57:23 +0800

    tabBar配置完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,33 @@
 //index.js
 import request from "../../utils/request.js"
 Page({
+  //页面加载触发
    onLoad(){
      //测试接口
      request({
        url:"/api/public/v1/home/swiperdata"
      }).then(res=>{
-       console.log(res)
+       
+       this.setData({
+         result:res.data
+       })
      })
-   }
+     console.log(this.data)
+   },
+  onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'page/component/pages/swiper/swiper'
+    }
+  },
+
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500,
+    result:{}
+  },
 })
