da79ca349562ec8d6e99f7b02c13d635274f4d93
SamzoGuo <53573565+SamzoGuo@users.noreply.github.com>
2019-10-31 16:17:18 +0800

    商品搜索页数据渲染

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,18 +1,38 @@
 // pages/goods_list/index.js
+import request from '../../utils/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    //参数
+    query:'',
+    //商品列表
+    goods:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      console.log(options)
+    //获取参数
+    const {query}=options
+    this.setData({
+      query
+    })
+    //获取商品列表
+    request({
+      url:'/api/public/v1/goods/search',
+      data:{
+        query:this.data.query
+      }
+    }).then(res=>{
+      this.setData({
+        goods:res.data.message.goods
+      })
+      console.log(this.data.goods)
+    })
   },
 
   /**
